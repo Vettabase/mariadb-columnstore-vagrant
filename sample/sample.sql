02d6eb633cbbb6f3e1a263bd56fea6af854f22b1
rcbensley <richardbensley@gmail.com>
2024-02-09 15:26:51 +0000

    Just one column for the sample

--- sample/sample.sql
+++ sample/sample.sql
@@ -2,9 +2,8 @@ CREATE SCHEMA IF NOT EXISTS sample;
 USE sample;
 
 CREATE TABLE IF NOT EXISTS sample.words (
-    id INT auto_increment,
     word VARCHAR(128)
 )ENGINE=Columnstore;
 DELETE FROM sample.words;
 
-LOAD DATA INFILE '/usr/share/dict/british-english-huge' INTO TABLE sample.words;
+LOAD DATA INFILE '/usr/share/dict/british-english-huge' INTO TABLE sample.words (word);
